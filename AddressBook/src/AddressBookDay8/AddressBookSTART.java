141156c7cedbb6d4b9a0eff250be11e31265c6ee
lalit-003 <gahlawatl7@gmail.com>
2020-10-06 03:35:52 +0530

    Book Start

---
+++ AddressBook/src/AddressBookDay8/AddressBookSTART.java
@@ -0,0 +1,11 @@
+package AddressBookDay8;
+
+// starting program to welcome into address problem
+public class AddressBookSTART {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to AddressBook !");
+
+	}
+}
